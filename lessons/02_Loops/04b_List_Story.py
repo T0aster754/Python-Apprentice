401aa6bc9bd777f8d63627fa0f1e3fd82b2ff16e
T0aster754 <164818999+T0aster754@users.noreply.github.com>
2025-05-17 01:28:04 +0000

    iugwe

--- lessons/02_Loops/04b_List_Story.py
+++ lessons/02_Loops/04b_List_Story.py
@@ -7,11 +7,13 @@ append them to the story
 """
 
 words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
-    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
+         'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
 
-story = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+story_indices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-# Create a story using the words in the list
+# Use indexing to get the words from the list
+print(words[0:1])
+print
 
-# Display the story to the user
-print(' '.join(story))
+# Display the story
+print(' '.join(words))
